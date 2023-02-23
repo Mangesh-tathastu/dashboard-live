75fa3ae118148fba0216ae4ca96d0f900c3007c1
Mangesh Balasaheb Niras <87422044+mangeshniras@users.noreply.github.com>
2023-02-23 18:23:30 +0530

    adding graph

--- JS/TBCompanyDashboard.js
+++ JS/TBCompanyDashboard.js
@@ -124,3 +124,130 @@ function ProjectDfunction() {
   function completedProjectfunction() {
     displaycomponents.innerHTML = completedProject()
   }
+
+
+  
+
+  /* ---------------------------------------------------
+    employee graph start 
+  ----------------------------------------------------- */
+  
+  const ctx1 = document.getElementById("bar-chart-grouped");
+
+  new Chart(ctx1 , {
+    type: "bar",
+    data: {
+      labels: ["Total Employees", "Total Projects"],
+      datasets: [
+        {
+          label: "Total",
+          backgroundColor: "#3e95cd",
+          data: [40, 22],
+        },
+        {
+          label:["Working","On Going"],
+          backgroundColor: " #16243d",
+          data: [35, 14],
+        },
+        {
+          label: ["Leave","Completed"],
+          backgroundColor: "#F15C44",
+          data: [5, 8],
+        },
+      ],
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Tathastu growth",
+      },
+    },
+  });
+  /* ---------------------------------------------------
+     Employee graph end
+  ----------------------------------------------------- */
+
+ /* ---------------------------------------------------
+     Project graph start
+  ----------------------------------------------------- */
+    
+  new Chart(document.getElementById("line-chart"), {
+    type: "line",
+    data: {
+      labels: [
+        "z",
+        "jan",
+        "Feb",
+        "Mar",
+        "apr",
+        "may",
+        "jun",
+        "jul",
+        "aug",
+        "sep",
+        "oct",
+        "nov",
+        "dec",
+      ],
+      datasets: [
+        {
+          data: [0, 7, 5, 11.13, 9],
+          label: "Total",
+          backgroundColor: "#3e95cd",
+          fill: false,
+        },
+        {
+          data: [0, 4, 3, 7, 8, 5],
+          label: "BIM",
+          backgroundColor: " #16243d",
+          fill: false,
+        },
+        {
+          data: [0, 3, 2, 4, 5, 4],
+          label: "Tech",
+          backgroundColor: "#F15C44",
+          fill: false,
+        },
+      ],
+    },
+    options: {
+      title: {
+        display: true,
+        text: "World population per region (in millions)",
+      },
+    },
+  });
+   /* ---------------------------------------------------
+     Project graph end
+  ----------------------------------------------------- */
+
+  /* ---------------------------------------------------
+     Circular graph start 
+  ----------------------------------------------------- */
+
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: ["Mangesh", "Niraj", "Vibhuti", "Yashvi"],
+    datasets: [
+      {
+        label: "Work Update hr",
+        data: [12, 10, 6, 9],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+/* ---------------------------------------------------
+     Circular graph end 
+  ----------------------------------------------------- */
